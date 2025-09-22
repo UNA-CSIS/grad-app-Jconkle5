efcecd514d72559e7e4546e00051bdf51fe0c2a9
Jconkle5 <jason.conkle5@gmail.com>
2025-09-21 22:37:16 -0500

    modified final class counter, added session destroy

--- aorpage.php
+++ aorpage.php
@@ -1,11 +1,22 @@
 <?php
     session_start();
+    
+    $countFull = count($_SESSION["classList"]);
+    $countComp = count($_SESSION["completed"]);
 
-    $count = count($_SESSION["completed"]);
+    if (($countComp / $countFull) >= 0.5) {
+        $count = true;
+    } else {
+        $count = false;
+    }
 
     if (stripos($_SESSION["saa"], "php") !== false) {
         $phpConf = true;
+    } else {
+        $phpConf = false;
     }
+
+    session_destroy();
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +25,7 @@
         <h1> Thank you for your submission</h1>
         <br><br>
         <?php
-            if ($count >= 3 && $phpConf === true) {
+            if ($count === true && $phpConf === true) {
                 echo ("You are accepted for a phone interview.");
             } else {
                 echo ("We are sorry, your application was rejected.");
